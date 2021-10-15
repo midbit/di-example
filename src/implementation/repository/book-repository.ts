1695de5d8f552f6d37de314cc10fb80c6dde2539
Midbit <midnightrabbit@protonmail.com>
2021-10-15 11:37:18 +0700

    Book-Repository

--- src/implementation/repository/book-repository.ts
+++ src/implementation/repository/book-repository.ts
@@ -1,26 +1,28 @@
 import {Book} from "../../domain/models/index.ts"
+import {BookRepositoryInterface} from "../../interfaces/repository/index.ts"
 
-class BookRepository {
+class BookRepository implements BookRepositoryInterface  {
 	private database = [
 		new Book(1, "The Little Prince", "Antoine De Saint-Exupery", true),
 		new Book(2, "Pride and Prejudice", "Jane Austen", true),
 		new Book(3, "Moby Dick", "Herman Melville", true)
 	]
 
-	find(id?: number): Book[] {
+	find(id?: number): Promise<Book[]> {
 		if(id) {
-			return this.database.filter((book) => id == book.id).sort((firstBook, secondBook) => firstBook.id < secondBook.id? 1: -1)
+			return Promise.resolve(this.database.filter((book) => id == book.id).sort((firstBook, secondBook) => firstBook.id < secondBook.id? 1: -1))
 		}
-		return this.database.sort((firstBook, secondBook) => firstBook.id < secondBook.id? -1: 1)
+		return Promise.resolve(this.database.sort((firstBook, secondBook) => firstBook.id < secondBook.id? -1: 1))
 	}
 
-	update(updatedBook: Book): void {
+	update(updatedBook: Book): Promise<void> {
 		const originalLength = this.database.length
 		const filteredLength= this.database.filter((book) => book.id != updatedBook.id)
 		if(originalLength === filteredLength.length) {
 			throw new Error("Book not found")
 		}
 		this.database = [...filteredLength, {...updatedBook}]
+		return Promise.resolve()
 	}
 }
 
